03f6950a7817e6bb660b9d9302e0a40476ce5195
Joaquincamps <joaquin.campsmartinez@educa.madrid.org>
2026-01-31 18:25:24 +0100

    Ejercicio de UDP cliente servidor solo y bastante elaborado

--- src/main/java/sockets/java/udp_monohilo/eje2/Cliente_2UDP.java
+++ src/main/java/sockets/java/udp_monohilo/eje2/Cliente_2UDP.java
@@ -1,4 +1,50 @@
 package sockets.java.udp_monohilo.eje2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Cliente_2UDP {
+
+    private static final int PUERTO = 5555;
+
+    public static void main(String[] args) {
+
+        try {
+            DatagramSocket cliente = new DatagramSocket();
+            System.out.println("Cliente UDP inciado");
+            InetAddress host = InetAddress.getByName("localhost");
+            System.out.println("Servidor " + host.getHostName() + ":" + PUERTO);
+            System.out.println("Comandos: ECO <texto> | MAYUS <texto> | LONG <texto> | SALIR");
+            System.out.println("----------------------------------------------------------");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+                String cad = br.readLine();
+                byte[] mensajeByte = cad.getBytes();
+
+                DatagramPacket peticion = new DatagramPacket(mensajeByte, mensajeByte.length,
+                        host, PUERTO);
+
+                cliente.send(peticion);
+
+                byte[] bufer = new byte[10000];
+                DatagramPacket mensaje = new DatagramPacket(
+                        peticion.getData(), peticion.getLength()
+                );
+                cliente.receive(mensaje);
+                System.out.println("Respuesta del servidor: " + new String(
+                        mensaje.getData(), 0, mensaje.getLength()
+                ));
+
+            }
+        } catch (Exception e) {
+            System.out.println("Erro con el usuario" + e.getMessage());
+        }
+
+
+    }
 }
--- src/main/java/sockets/java/udp_monohilo/eje2/Servidor_2UDP.java
+++ src/main/java/sockets/java/udp_monohilo/eje2/Servidor_2UDP.java
@@ -1,4 +1,67 @@
 package sockets.java.udp_monohilo.eje2;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class Servidor_2UDP {
+
+    private static final int PUERTO = 5555;
+
+    public static void main(String[] args) {
+
+        try {
+            DatagramSocket servidor = new DatagramSocket(PUERTO);
+            System.out.println("Servidor UPD iniciado en el puerto " + PUERTO);
+            byte[] buffer = new byte[10000];
+
+            while (true) {
+                DatagramPacket peticion = new DatagramPacket(
+                        buffer, buffer.length
+                );
+                servidor.receive(peticion);
+                String mensajeEnTexto = new String(peticion.getData(), 0, peticion.getLength());
+
+
+                if (!mensajeEnTexto.contains("ECO") && !mensajeEnTexto.contains("MAYUS")
+                        && !mensajeEnTexto.contains("LONG")) {
+                    break;
+                }
+
+                System.out.println("Recibido de " + peticion.getAddress() + ":"
+                        + peticion.getPort() + " -> " + mensajeEnTexto);
+
+                String[] partes = mensajeEnTexto.split(" ");
+
+                DatagramPacket mensaje;
+                byte[] bufferRespuesta;
+
+                if (mensajeEnTexto.contains("ECO")) {
+                    bufferRespuesta = partes[1].getBytes();
+
+                    mensaje = new DatagramPacket(bufferRespuesta, bufferRespuesta.length,
+                            peticion.getAddress(), peticion.getPort());
+                    servidor.send(mensaje);
+                }
+                if (mensajeEnTexto.contains("MAYUS")) {
+                    String mensajeEco = partes[1].toUpperCase();
+                    bufferRespuesta = mensajeEco.getBytes();
+
+                    mensaje = new DatagramPacket(bufferRespuesta, bufferRespuesta.length,
+                            peticion.getAddress(), peticion.getPort());
+                    servidor.send(mensaje);
+                }
+                if (mensajeEnTexto.contains("LONG")) {
+                    String mensajeEco = String.valueOf(partes[1].length());
+                    bufferRespuesta = mensajeEco.getBytes();
+
+                    mensaje = new DatagramPacket(bufferRespuesta, bufferRespuesta.length,
+                            peticion.getAddress(), peticion.getPort());
+                    servidor.send(mensaje);
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error al innciar el servidor UDP" + e.getMessage());
+        }
+    }
 }
